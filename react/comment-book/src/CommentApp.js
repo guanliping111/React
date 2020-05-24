1da021cb19b7c4b633231633f873deeb43fadf7d
guanliping <2289029828@qq.com>
2020-05-24 09:24:41 +0800

    add react5 评论功能

--- react/comment-book/src/CommentApp.js
+++ react/comment-book/src/CommentApp.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
+
+
 class CommentApp extends Component {
   constructor () {
     super()
@@ -11,9 +13,39 @@ class CommentApp extends Component {
   }
 
 
-  handleSubmitComment (comment) {
-    
+  componentWillMount () {
+    this._loadComments()
   }
+
+
+  _loadComments () {
+    let comments = localStorage.getItem('comments')
+    if (comments) {
+      comments = JSON.parse(comments)
+      this.setState({ comments })
+    }
+  }
+
+
+  _saveComments (comments) {
+    localStorage.setItem('comments', JSON.stringify(comments))
+  }
+
+
+  handleSubmitComment (comment) {
+    // console.log(comment);
+    //判断是否输入
+    if (!comment) return
+    if (!comment.username) return alert('请输入用户名')
+    if (!comment.content) return alert('请输入评论内容')
+    const comments = this.state.comments // old state
+    comments.push(comment)
+    this.setState({ comments })
+    this._saveComments(comments)
+  }
+
+
+
   render() {
     return (
       <div className='wrapper'>
@@ -25,5 +57,6 @@ class CommentApp extends Component {
     )
   }
 }
+
 
 export default CommentApp;
