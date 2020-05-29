31979e8cc6f3adc30e86048334056cff04abc2d1
guanliping <2289029828@qq.com>
2020-05-29 09:36:06 +0800

    add 渲染列表map

--- react/react-book1/src/index.js
+++ react/react-book1/src/index.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+const users = [
+  { username: 'Jerry', age: 21, gender: 'male' },
+  { username: 'Tomy', age: 22, gender: 'male' },
+  { username: 'Lily', age: 19, gender: 'female' },
+  { username: 'Lucy', age: 20, gender: 'female' }
+]
 
-class LikeButton extends Component {
-  constructor () {
-    super()
-    this.state = { isLiked: false }
-  }
-
-  handleClickOnLikeButton () {
-    this.setState({
-      isLiked: !this.state.isLiked
-    })
-  }
-
+class User extends Component {
   render () {
-    const likedText = this.props.likedText || '取消'
-    const unlikedText = this.props.unlikedText || '点赞'
+    const { user } = this.props
     return (
-      <button onClick={this.handleClickOnLikeButton.bind(this)}>
-        {this.state.isLiked ? likedText : unlikedText} 👍
-      </button>
+      <div>
+        <div>姓名：{user.username}</div>
+        <div>年龄：{user.age}</div>
+        <div>性别：{user.gender}</div>
+        <hr />
+      </div>
     )
   }
 }
+
 class Index extends Component {
   render () {
     return (
       <div>
-        <LikeButton />
+        {users.map((user, i) => <User key={i} user={user} />)}
       </div>
     )
   }
