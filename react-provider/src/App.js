97c2d32c11668c600b43dfcb0aa3b5a4307f0132
guanliping <2289029828@qq.com>
2020-06-10 14:40:16 +0800

    add react

---
+++ react-provider/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Headers />
+      <footer />
+    </Fragment>
+    
+  );
+}
+
+export default App;
