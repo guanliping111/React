a7fc9ecd1ecafcaa3338179126a1751cd522a5a0
guanliping <2289029828@qq.com>
2020-06-18 16:48:27 +0800

    update reect-mobx

--- react-mobx-realworld/src/App.js
+++ react-mobx-realworld/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import { Provider } from 'mobx-react';
+import articleStore from './store/articleStore';
+import Home from './pages/home';
 import logo from './logo.svg';
+import './util/request';
 import './App.css';
 
+const store = {
+  articleStore
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider {...store}>
+      <Home />
+    </Provider>
   );
 }
 
