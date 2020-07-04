6b940e945cf5e5bce2e9a4be3f29f6bdb686b9c7
guanliping <2289029828@qq.com>
2020-07-04 14:40:30 +0800

    add redux中的重要部分

--- redux-demo/src/index.js
+++ redux-demo/src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { func } from 'prop-types';
-import { get } from 'axios';
-import thunk from 'redux-thunk';
+import { get } from 'axios'
+import thunk from 'redux-thunk'
 
-//redux中的重要部分 action reducer state 
-const cousnterReducer = function(state={ count : 1}, action ) {
+// redux中的重要部分：action，reducer, state
+const counterReducer = function(state = { count: 1 }, action) {
   switch(action.type) {
     case 'COUNT_ADD':
-    return {
-      ...state, count: state.count + 1
-    }
+      return {
+        ...state, count: state.count + 1
+      }
     case 'COUNT_REDUCE':
       return {
         ...state, count: state.count - 1
@@ -23,53 +22,65 @@ const cousnterReducer = function(state={ count : 1}, action ) {
       return state
   }
 } 
- const postReducer = function(state={ list: [{title: '您好'}]}, action ) {
-    switch(action.type) {
-      case 'LOAD_POATS':
-        return {
-          ...state, list: action.payload
-        }
-      default:
-        return state
-    }
- }
 
- //combineReducers 用来整合多个reducer
- const rootReducers = combineReducers({
-   counter:combineReducers,
-   post: postReducer
- })
+const postReducer = function(state = { list: [{title: '你好'}]}, action) {
+  switch(action.type) {
+    case 'LOAD_POSTS':
+      return {
+        ...state, list: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+// combineReducers用来整合多个reducer
+const rootReducers = combineReducers({
+  counter: counterReducer,
+  post: postReducer
+})
 
 const store = createStore(
-  cousnterReducer,
+  rootReducers,
   compose(
-    applyMiddleware(
-      ...[thunk]), //需要使用中间件的数组
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...[thunk]), // 需要使用中间件数组
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-  ); //创建一个仓库store
-console.log(store);
-console.log(store.getState);
+) // 创建一个store
 
-//想要改变reducer 的值 ，需要使用dispatch 派发一个action
-//action里面需要两个参数 type payload
+console.log(store)
+console.log(store.getState())
+
+// 想要改变reducer的值，需要使用dispatch派发一个action
+// action里面需要两个参数   type   payload
+
 store.dispatch({
-  type:'COUNT_REDUCE',
+  type: 'COUNT_ADD',
   payload: {},
 })
 
-console.log(store);
-console.log(store.getState);
+console.log(store)
+console.log(store.getState())
 
-const getPostRequest = () =>{
-  return get('https://jsonplaceholder.typicode.com/posts');
+store.dispatch({
+  type: 'COUNT_REDUCE',
+  payload: {},
+})
+
+console.log(store)
+console.log(store.getState())
+
+
+const getPostRequest = () => {
+  return get('https://jsonplaceholder.typicode.com/posts')
 }
+
 store.dispatch(async function(dispatch) {
-  const res = await getPostRequest();
-  console.log(res);
+  const res = await getPostRequest()
+  console.log(res)
   dispatch({
     type: 'LOAD_POSTS',
-    payload: res.date
+    payload: res.data
   })
 })
 
@@ -85,3 +96,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
